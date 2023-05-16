bb7f58d0aaf76922525025410169b9517cea7a45
Nitza Alfinas <nitzaalfinas@gmail.com>
2023-05-16 20:30:52 +0700

    perbaikan

--- app/controllers/v1/profiles_controller.rb
+++ app/controllers/v1/profiles_controller.rb
@@ -24,33 +24,57 @@ module V1
     
     def update
 
-      @profile = Profile.find_by_user_id(decoded_auth_token[:user_id])
-      @profile.full_name     = params[:full_name]
-      @profile.phone_number  = params[:phone_number]
-      @profile.address       = params[:address]
-      @profile.id_number     = params[:id_number]
-      @profile.npwp_number   = params[:npwp_number]
-      @profile.country       = params[:country]
+      user_id = decoded_auth_token[:user_id]
 
-      if params[:file_npwp] && params[:file_npwp] != ""
-        @profile.file_npwp = params[:file_npwp]
+      @profile = Profile.find_by_user_id(user_id)
+
+      if @profile.status_kyc == false 
+
+        @profile.full_name     = params[:full_name]
+        @profile.phone_number  = params[:phone_number] 
+        @profile.address       = params[:address]
+        @profile.id_number     = params[:id_number]
+        @profile.npwp_number   = params[:npwp_number]
+        @profile.country       = params[:country]
+
+        if params[:file_npwp] && params[:file_npwp] != ""
+          @profile.file_npwp = params[:file_npwp]
+        end
+
+        if params[:file_ktp] && params[:file_ktp] != ""
+          @profile.file_ktp = params[:file_ktp]
+        end
+
+        if params[:image] && params[:image] != ""
+          @profile.image = params[:image]
+        end
+
+        if @profile.save
+
+          render json: {
+            success: true, 
+            msg: 'Profiles is update', 
+            data: ActiveModelSerializers::SerializableResource.new(@profile, each_serializer: ProfileSerializer)
+          }, status: :ok
+        else 
+          if @profile.errors?
+            render json: {
+              status: false,
+              msg: @profile.errors.to_json
+            }
+          else  
+            render json: {
+              status: false,
+              msg: 'Unknown error'
+            }
+          end
+        end
+      else 
+        render json: {
+          status: false,
+          msg: 'KYC gidak bole di edit'
+        }
       end
-
-      if params[:file_ktp] && params[:file_ktp] != ""
-        @profile.file_ktp = params[:file_ktp]
-      end
-
-      if params[:image] && params[:image] != ""
-        @profile.image = params[:image]
-      end
-
-      @profile.save
-
-      render json: {
-        success: true, 
-        msg: 'Profiles is update', 
-        data: ActiveModelSerializers::SerializableResource.new(@profile, each_serializer: ProfileSerializer)
-      }, status: :ok
     end
 
   
