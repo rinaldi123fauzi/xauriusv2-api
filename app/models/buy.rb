0d760fa229219af0a58f53a25fcf9ff2f6ae5015
Nitza Alfinas <nitzaalfinas@gmail.com>
2023-06-07 20:35:51 +0700

    Update buy.rb

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -9,6 +9,11 @@
 #  updated_at :datetime         not null
 #  user_id    :bigint           not null
 #
+
+# TODO: amount_idr, amount_xau
+# price => bisa diambil dari harga database charts (kalau gak salah)
+# summary => hasil kalkulasi harga dan jumlah_idr_pembelian (diganti jadi amount_xau)
+# 
 class Buy < ApplicationRecord
     belongs_to :user
 end
