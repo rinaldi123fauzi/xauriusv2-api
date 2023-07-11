c6430215452d1f61d785b9c774f4739cd7a14225
Mohamad Rinaldi Fauzi <rinalfauzi@gmail.com>
2023-07-11 20:44:25 +0700

    settings otp expired

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_084938) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_131152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -228,6 +228,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_084938) do
     t.bigint "otp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expired_at", precision: nil
   end
 
   create_table "profiles", force: :cascade do |t|
